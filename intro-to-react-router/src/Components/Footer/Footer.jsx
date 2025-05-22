908f86377077279fa6e4f929cc1661c1c20d2c17
Md <dipum0505@gmail.com>
2025-05-22 10:13:04 +0600

    first commit

---
+++ intro-to-react-router/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div>
+            <p>
+             <small>Thank you for visiting our website</small>
+             <a href="/">Terms</a>  
+             <a href="/">Privacy</a>  
+             <a href="/">Facebook</a>  
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
